b228e1b9441e0e614658014f7bb12bca791831d9
Matthew Vassilieff <101568041+Astr0cyte@users.noreply.github.com>
2026-07-21 11:36:49 +0700

    Final bugfixes 2

--- lib/pages/diary_page.dart
+++ lib/pages/diary_page.dart
@@ -1,5 +1,6 @@
 import 'package:brewstreet_app/app_colors.dart';
 import 'package:brewstreet_app/pages/saved_shops.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../model/diary_note.dart';
 import '/widgets/add_note_dialog.dart';
@@ -20,28 +21,41 @@ class _DiaryPageState extends State<DiaryPage> {
 
   int selectedTab = 3;
 
-final List<DiaryNote> notes = [
-  DiaryNote(
-    title: "Coffee tasting notes",
-    body:
-        "• Longer note has a drop down and ellipse\n"
-        "to demonstrate a note is longer than the\n"
-        "\"preview\" card.\n"
-        "• Arabica beans.\n"
-        "• Toffee praline flavours. Earthy richness.",
-    expanded: true,
-  ),
-  DiaryNote(
-    title: "Shorter note",
-    body: "• No drop-down required for short notes.",
-  ),
-  DiaryNote(
-    title: "Coffee tasting notes",
-    body:
-        "• Ability to attach notes to specific coffee\n"
-        "shop items within a specific coffee shop?",
-  ),
-];
+  final List<DiaryNote> notes = [
+    DiaryNote(
+      title: "Coffee tasting notes",
+      body: "• Longer note has a drop down and ellipse\n"
+          "to demonstrate a note is longer than the\n"
+          "\"preview\" card.\n"
+          "• Arabica beans.\n"
+          "• Toffee praline flavours. Earthy richness.",
+      expanded: true,
+    ),
+    DiaryNote(
+      title: "Shorter note",
+      body: "• No drop-down required for short notes.",
+    ),
+    DiaryNote(
+      title: "Coffee tasting notes",
+      body: "• Ability to attach notes to specific coffee\n"
+          "shop items within a specific coffee shop?",
+    ),
+  ];
+  Stream<List<DiaryNote>> loadFirebaseNotes() {
+    return FirebaseFirestore.instance
+        .collection("notes")
+        .snapshots()
+        .map((snapshot) {
+      return snapshot.docs.map((doc) {
+        final data = doc.data();
+
+        return DiaryNote(
+          title: data["notesTitle"] ?? "",
+          body: data["notesDescription"] ?? "",
+        );
+      }).toList();
+    });
+  }
 
   final tabs = const [
     "Glossary",
@@ -77,21 +91,17 @@ final List<DiaryNote> notes = [
             fontFamily: GoogleFonts.quicksand().fontFamily,
           ),
         ),
-
         const Align(
-            alignment: Alignment(0, -0.50),
-            child: Divider(
-              color: AppColors.gold,
-              thickness: 1,
-            ),
+          alignment: Alignment(0, -0.50),
+          child: Divider(
+            color: AppColors.gold,
+            thickness: 1,
           ),
-
+        ),
         const SizedBox(height: 16),
-
         StreamBuilder<List<DiaryNote>>(
           stream: loadFirebaseNotes(),
           builder: (context, snapshot) {
-
             final firebaseNotes = snapshot.data ?? [];
 
             final allNotes = [
@@ -113,8 +123,7 @@ final List<DiaryNote> notes = [
                       });
                     },
                     onEdit: () async {
-                      final DiaryNote? editedNote =
-                          await showDialog<DiaryNote>(
+                      final DiaryNote? editedNote = await showDialog<DiaryNote>(
                         context: context,
                         builder: (_) => AddNoteDialog(),
                       );
@@ -132,9 +141,7 @@ final List<DiaryNote> notes = [
             );
           },
         ),
-
         const SizedBox(height: 22),
-
         GestureDetector(
           onTap: () async {
             final DiaryNote? newNote = await showDialog<DiaryNote>(
